5f38e9cd4ae9767eb21b7d7f29e85e5b32e0ef53
Arijit Dey <arijit.dey18@icloud.com>
2019-08-22 15:40:27 -0700

    Updating to terraform 0.12

--- alarms.tf
+++ alarms.tf
@@ -1,12 +1,14 @@
 locals {
   thresholds = {
-    BurstCreditBalanceThreshold = "${max(var.burst_credit_balance_threshold, 0)}"
-    PercentIOLimitThreshold     = "${min(max(var.percent_io_limit_threshold, 0), 100)}"
+    BurstCreditBalanceThreshold = max(var.burst_credit_balance_threshold, 0)
+    PercentIOLimitThreshold     = min(max(var.percent_io_limit_threshold, 0), 100)
   }
 
   alert_for     = "efs"
-  sns_topic_arn = "${var.sns_topic_arn == "" ? aws_sns_topic.default.arn : var.sns_topic_arn }"
-  endpoints     = "${distinct(compact(concat(list(local.sns_topic_arn), var.additional_endpoint_arns)))}"
+  sns_topic_arn = var.sns_topic_arn == "" ? aws_sns_topic.default.arn : var.sns_topic_arn
+  endpoints = distinct(
+    compact(concat([local.sns_topic_arn], var.additional_endpoint_arns)),
+  )
 }
 
 resource "aws_cloudwatch_metric_alarm" "burst_credit_balance_too_low" {
@@ -17,13 +19,13 @@ resource "aws_cloudwatch_metric_alarm" "burst_credit_balance_too_low" {
   namespace           = "AWS/EFS"
   period              = "600"
   statistic           = "Average"
-  threshold           = "${local.thresholds["BurstCreditBalanceThreshold"]}"
+  threshold           = local.thresholds["BurstCreditBalanceThreshold"]
   alarm_description   = "Average burst credit balance over last 10 minutes too low, expect a significant performance drop soon"
-  alarm_actions       = ["${local.endpoints}"]
-  ok_actions          = ["${local.endpoints}"]
+  alarm_actions       = local.endpoints
+  ok_actions          = local.endpoints
 
-  dimensions {
-    FileSystemId = "${var.filesystem_id}"
+  dimensions = {
+    FileSystemId = var.filesystem_id
   }
 }
 
@@ -35,12 +37,12 @@ resource "aws_cloudwatch_metric_alarm" "percent_io_limit_too_high" {
   namespace           = "AWS/EFS"
   period              = "600"
   statistic           = "Maximum"
-  threshold           = "${local.thresholds["PercentIOLimitThreshold"]}"
+  threshold           = local.thresholds["PercentIOLimitThreshold"]
   alarm_description   = "I/O limit has been reached, consider using Max I/O performance mode"
-  alarm_actions       = ["${local.endpoints}"]
-  ok_actions          = ["${local.endpoints}"]
+  alarm_actions       = local.endpoints
+  ok_actions          = local.endpoints
 
-  dimensions {
-    FileSystemId = "${var.filesystem_id}"
+  dimensions = {
+    FileSystemId = var.filesystem_id
   }
 }
